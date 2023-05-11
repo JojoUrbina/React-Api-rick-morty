22c286ff5a2e1223d092582853e25ce62bf4fe30
Jorman Urbina <jormanurbina1@gmail.com>
2023-05-11 19:28:56 -0300

    se agregaron botones para cambiar pagina y se agrego archivo deploy.sh

--- components/CharactersList.jsx
+++ components/CharactersList.jsx
@@ -1,31 +1,49 @@
-
 import { useEffect, useState } from "react";
-import Character from "./Character"
+import Character from "./Character";
+
+import React from "react";
+
+export function NavPage(props) {
+  return (
+    <header className="d-flex justify-content-between align-items-center  ">
+      <p>page:{props.page}</p>
+      <button
+        onClick={() => props.setPage(props.page + 1)}
+        className="btn btn-primary btn-sm"
+      >
+        page {props.page + 1}
+      </button>
+      {/* <button onClick={()=>props.setPage(props.page-1) } className="btn btn-primary btn-sm">page {props.page-1}</button> */}
+    </header>
+  );
+}
 
 export default function CharactersList() {
-    const [characters, setCharacters] = useState([]);
-    useEffect(() => {
-      async function fetchData() {
-        const response = await fetch("https://rickandmortyapi.com/api/character");
-        const data = await response.json();
-        setCharacters(data.results);
-      }
-      fetchData();
-      
-    }, []);
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${page}`
+      );
+      const data = await response.json();
+      setCharacters(data.results);
+    }
+    fetchData();
+  }, [page]);
 
   return (
     <div className="container ">
-        <div className="row">
-        {characters.map(character=>{
-        return(
+      <NavPage page={page} setPage={setPage} />
+      <div className="row">
+        {characters.map((character) => {
+          return (
             <div className="col-md-4 " key={character.id}>
-            <Character  character={character}/>
+              <Character character={character} />
             </div>
-        )
-        
-      })}
+          );
+        })}
       </div>
-      </div>
-  )
+    </div>
+  );
 }
