4644b5da89077db261033b3ac48756a20a162c00
Jorman Urbina <jormanurbina1@gmail.com>
2023-05-11 21:40:05 -0300

    se actualizaron textos

--- components/CharactersList.jsx
+++ components/CharactersList.jsx
@@ -6,12 +6,12 @@ import React from "react";
 export function NavPage(props) {
   return (
     <header className="d-flex justify-content-between align-items-center  ">
-      <p>page:{props.page}</p>
+      <p>pagina actual:{props.page}</p>
       <button
         onClick={() => props.setPage(props.page + 1)}
         className="btn btn-primary btn-sm"
       >
-        page {props.page + 1}
+        pagina {props.page + 1}
       </button>
       <button onClick={()=>props.setPage(props.page>1?props.page-1:1) } className="btn btn-primary btn-sm">Atras</button>
     </header>
