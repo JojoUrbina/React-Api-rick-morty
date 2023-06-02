40693fd86df72d76a70f08d0527b2dcf3913cd6b
Jorman Urbina <jormanurbina1@gmail.com>
2023-06-02 19:15:16 -0300

    se actualizo enlace base por error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://JojoUrbina.github.io/react-rickandmorty/",
+  base:"https://JojoUrbina.github.io/React-Api-rick-morty/"
+  ,
   plugins: [react()],
 })
